747af71300396e12af5604f1c9a060153cb809ef
lewycaron <lewycaron@gmail.com>
2022-06-10 00:44:57 -0300

    arquivos organizados

---
+++ 2-bimestre/Bootstrap e Javascript/javascript 02/script.js
@@ -0,0 +1,5 @@
+alert('Olá mundo loko!')
+
+var nome = prompt('Digite o seu nome: ')
+var idade = prompt('Digite a sua idade: ')
+document.write('<h1>Olá ' + nome + ', tudo bem? É verdade que você tem ' + idade + ' anos?</h1>')
